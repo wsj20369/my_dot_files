e5bdfff6d1d824b8a2655be82b3d9a284e9c49f0
shujun <wsj20369@foxmail.com>
2021-03-11 14:22:52 +0800

    Add script to install basic packages after newly Ubuntu Installed

---
+++ do-apt-install-for-new-ubuntu.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+sudo apt install \
+	net-tools openssh-server git tmux vim wget curl tree pv htop gettext unzip  \
+	zsh make automake autoconf autopoint gcc flex bison build-essential \
+	openssl libssl-dev libelf-dev libncurses5-dev kernel-package libgtk2.0-dev \
+	libgnutls28-dev libxpm-dev libjpeg-dev libgif-dev libtiff-dev
+
